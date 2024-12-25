575627c249371c9f620ac85a401d282cdf4075a5
Ronin <bhabyaprakash01@gmail.com>
2024-12-25 18:35:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Aromaroy/Aroma",
+    "https://github.com/Roninbyrne/Aromaa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
