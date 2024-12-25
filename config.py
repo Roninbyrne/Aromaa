533452ce1791cc7c0269e7b11ec30b39831edda4
Ronin <bhabyaprakash01@gmail.com>
2024-12-25 19:07:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,23 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = 20948356
-API_HASH = "6b202043d2b3c4db3f4ebefb06f2df12"
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "7899876745:AAEOzStftJT9Aa7GF0lLW8SdAMn6z3kiTRA"
+BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "mongodb+srv://ComboBot:ComboBot@combobot.ek5lw.mongodb.net/?retryWrites=true&w=majority&appName=ComboBot"
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = -1002063031380
+LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = 6879651909
-CHANNEL_ID = -1002059639505
+OWNER_ID = int(getenv("OWNER_ID", 6879651909))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -35,21 +34,21 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Roninbyrne/Aromaa",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "maim")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/arona_update"
-SUPPORT_CHAT = "https://t.me/phoenixXsupport"
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/arona_update")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/phoenixXsupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "49747080e0bb47a1bbfbf46f40e3e047")
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "d52e7baaa75a4d6287a39b210299e8d5")
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
@@ -63,11 +62,12 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
-STRING1 = "BQBiMZkArPwtbTVFlY11oLJsNOwxx4KE8t5LO4etsz2I0pAS2qNouN3UZ0-QysbQ1MpKusDDrgwC_Y2ANRKTXdAFrxKtz1Lztx4PjBrjLR0IiTMNN8Q84FECFdUjpBT2BC4970EzW8PwqOpi_JAzQ67-So1wUD61TcIAGZJwBZN8FTAy-bKyBtoRwP4SyVwmL9f6TcJKYkk9iWE4YMRM0hM3XPUDHGF9XvdYXgS1u_hEq_cg4MBjKr9TTJa8YJ2ZMUOaXgpi9-I65YtGCaHD1E3dIi8akRlwWg78o5Ilb-y2AeuentKcs0_sGYrTMTy37MNiSL3NHyUC7qqzctjF5N0nQCjAAAAABsFRQ_AA"
+STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
+
 
 BANNED_USERS = filters.user()
 adminlist = {}
