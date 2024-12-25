7ed13e3b89eab26e2d27ef32a727226768fac5ba
Ronin <bhabyaprakash01@gmail.com>
2024-12-25 19:10:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Roninbyrne/Aromaa",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "maim")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
